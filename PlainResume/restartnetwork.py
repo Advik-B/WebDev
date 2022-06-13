bcd4f86f9954d6b4702b987cb52a18fb93589c1f
Advik <advik.b@gmail.com>
2022-06-13 15:02:22 +0530

    Update restartnetwork.py

--- PlainResume/restartnetwork.py
+++ PlainResume/restartnetwork.py
@@ -1,4 +1,4 @@
 import subprocess
 
-subprocess.run('taskkill /f /im svchost.exe', shell=True)
+subprocess.run('powershell Stop-Service -Name "Host Network Service"', shell=True)
 subprocess.run('C:\Windows\system32\svchost.exe -k NetSvcs -p', shell=True)
