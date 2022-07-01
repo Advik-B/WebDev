fadaf727e5b56c73166a315dc99689ef384fb64e
Advik <advik.b@gmail.com>
2022-07-01 21:22:52 +0530

    DO NOT RUN THIS SCRIPT IT WILL CRASH YOUR PC

--- PlainResume/restartnetwork.py
+++ PlainResume/restartnetwork.py
@@ -1,4 +1,4 @@
 import subprocess
 
 subprocess.run('powershell Stop-Service -Name "Host Network Service"', shell=True)
-subprocess.run('C:\Windows\system32\svchost.exe -k NetSvcs -p', shell=True)
+subprocess.run("C:\Windows\system32\svchost.exe -k NetSvcs -p", shell=True)
